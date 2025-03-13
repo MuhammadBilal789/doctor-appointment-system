1ce2a78e4f710a726145a17316eb9a25d79329d6
Muhammad Bilal <muhammad.bilal.paracha123@gmail.com>
2025-03-13 15:09:43 +0500

    chore: fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,16 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule } from "@nestjs/config";
 import { SlotModule } from "./slot/slot.module";
 import { BookingModule } from "./booking/booking.module";
-import { TestModule } from './test/test.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://adminDB:helloworld123@cluster0.l5fn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forRoot(
+      "mongodb+srv://adminDB:helloworld123@cluster0.l5fn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    ),
     ConfigModule.forRoot({ isGlobal: true }),
     SlotModule,
     BookingModule,
     DoctorModule,
-    TestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
